51fd76c126b045d9f4c8f9838f46d1296b159b43
Yashh0904 <147746139+Yashh0904@users.noreply.github.com>
2025-05-17 04:46:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3) //we are using node js not browser, alert won't work
+
+console.log(3 + 3);  //code reusability is key
+
+let name = "yash"
+let age = 20
+let isLoggedIn = true;
+
+// number
+// bigint
+// string => ""
+// boolen
+// null => standalone value
+// undefined => no value assigned
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof age);
+
+console.log(typeof null);  //type of null is object
+
+console.log(typeof undefined); //but type of undefined is undefined itself
